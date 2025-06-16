b9491c14bc783038d85c2217bd6576e38ae5c142
BogdanAvd <bodya.byter@gmail.com>
2025-06-16 22:02:28 +0300

    All

--- Chat/src/ClientHandler.java
+++ Chat/src/ClientHandler.java
@@ -4,96 +4,99 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.HashSet;
-import java.util.Scanner;
 import java.util.Set;
 
+public class ClientHandler implements Runnable {
 
-class ClientHandler implements Runnable {
-        private Socket socket;
-        private String name;
-        private PrintWriter out;
-        private static Set<PrintWriter> clientWriters = new HashSet<>();
-        private BufferedReader in;
+    // === Поля ===
+    private Socket socket;
+    private String name;
+    private PrintWriter out;
+    private BufferedReader in;
+    private static final Set<PrintWriter> clientWriters = new HashSet<>();
 
-        ClientHandler (Socket socket) {
-            this.socket = socket;
-        }
-
-        public void startClientHandler() {
-            createReaderFromSocket(socket);
-            changeName();
-            addClientToSet();
-            broadcastMessage("До чату приєднався новий учасник! Привітайте " + name + "!");
-            showClientMessageForAll();
-            closeClient();
-        }
-
-        private void changeName() {
-            try {
-                name = in.readLine();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-        
-        public void addClientToSet() {
-            createPrintWriter(socket);
-            synchronized(clientWriters) {
-                clientWriters.add(out);
-            }
-        }   
-        
-        private void createPrintWriter(Socket socket) {
-            try {
-                out = new PrintWriter(socket.getOutputStream(), true);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-
-        public void createReaderFromSocket(Socket socket){
-            try {
-                in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-
-        private void broadcastMessage(String msg) {
-            synchronized(clientWriters) {
-                for (PrintWriter writer : clientWriters) {
-                    writer.println(msg);
-                }
-            }
-        }
-
-        public void showClientMessageForAll() {
-            String msg;
-            try {
-                while ((msg = in.readLine()) != null) {
-                    broadcastMessage(msg);
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-
-        public void closeClient() {
-            try {
-                socket.close();
-            } catch(IOException e) {
-                e.printStackTrace();
-            }
-            synchronized(clientWriters) {
-                clientWriters.remove(out);
-            }
-        }
-
-        
- 
-        @Override
-        public void run() {
-            startClientHandler();
-        }
-        
+    // === Конструктор ===
+    public ClientHandler(Socket socket) {
+        this.socket = socket;
     }
+
+    // === Основний життєвий цикл клієнта ===
+    @Override
+    public void run() {
+        startClientHandler();
+    }
+
+    public void startClientHandler() {
+        createReaderFromSocket(socket);
+        changeName();
+        addClientToSet();
+        broadcastMessage("До чату приєднався новий учасник! Привітайте " + name + "!");
+        showClientMessageForAll();
+        closeClient();
+    }
+
+    // === Робота з потоками вводу/виводу ===
+    private void createReaderFromSocket(Socket socket) {
+        try {
+            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void createPrintWriter(Socket socket) {
+        try {
+            out = new PrintWriter(socket.getOutputStream(), true);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // === Робота з іменем клієнта ===
+    private void changeName() {
+        try {
+            name = in.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // === Додавання/видалення клієнта зі списку ===
+    public void addClientToSet() {
+        createPrintWriter(socket);
+        synchronized (clientWriters) {
+            clientWriters.add(out);
+        }
+    }
+
+    public void closeClient() {
+        try {
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        synchronized (clientWriters) {
+            clientWriters.remove(out);
+        }
+    }
+
+    // === Робота з повідомленнями ===
+    private void broadcastMessage(String msg) {
+        synchronized (clientWriters) {
+            for (PrintWriter writer : clientWriters) {
+                writer.println(msg);
+            }
+        }
+    }
+
+    public void showClientMessageForAll() {
+        String msg;
+        try {
+            while ((msg = in.readLine()) != null) {
+                broadcastMessage(msg);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
