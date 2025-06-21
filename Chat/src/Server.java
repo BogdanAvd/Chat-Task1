d0ef3df0ffc548f5bdbec8d9e8c91ac5aca0e1db
BogdanAvd <bodya.byter@gmail.com>
2025-06-21 22:39:58 +0300

    Fixed stop button

--- Chat/src/Server.java
+++ Chat/src/Server.java
@@ -6,39 +6,45 @@ public class Server {
     private static final int PORT = 12345;
     private ServerSocket serverSocket;
     private Socket clientSocket;
+    private volatile boolean running = false;
 
     public void startServerSocket() {
         try {
             serverSocket = new ServerSocket(PORT);
+            running = true;
+            System.out.println("Server start!");
+            startAcceptClients();
         } catch (IOException e) {
             e.printStackTrace();
         }
-        System.out.println("Server start!");
-        startAcceptClients();
     }
 
     private void startAcceptClients() {
-        while(true) {   
+        while (running) {
             try {
                 clientSocket = serverSocket.accept();
+                passClientToStream(clientSocket);
             } catch (IOException e) {
-                e.printStackTrace();
+                if (running) {
+                    e.printStackTrace();
+                }
+                // Если сервер закрыт, выбрасывается исключение - можно выйти из цикла
+                break;
             }
-            passClientToStream(clientSocket);
         }
     }
 
     private void passClientToStream(Socket clientSocket) {
         new Thread(new ClientHandler(clientSocket)).start();
     }
-    
+
     public void stopServerSocket() {
+        running = false;
         try {
             if (serverSocket != null && !serverSocket.isClosed()) {
                 serverSocket.close();
-                System.out.println("Server stopped!");
             }
-        } catch (IOException e) {
+        } catch(IOException e) {
             e.printStackTrace();
         }
     }
